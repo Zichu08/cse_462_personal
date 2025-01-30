8cab530834f35cae501cad0ab71b1483dbf52c62
Zichu08 <143557155+Zichu08@users.noreply.github.com>
2025-01-30 04:58:44 -0600

    m1

--- rest_interface/views.py
+++ rest_interface/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from pynq import Overlay
+
+overlay = Overlay('/home/xilinx/pynq/calculator/design_1_wrapper.bit')
+calculator_ip = overlay.calculator_0
 
 class home(APIView):
     def get(self, request):
@@ -8,11 +12,26 @@ class home(APIView):
     
     def post(self, request):
         try:
-            input = int(request.data.get('input'))
-            result = self.compute(input)
+            input1 = int(request.data.get('input1'))
+            input2 = int(request.data.get('input2'))
+            operation = str(request.data.get('operation'))
+            result = self.compute(input1, input2, operation)
             return Response(result, status=status.HTTP_200_OK);
         except:
             return Response("POST error", status=status.HTTP_400_BAD_REQUEST)    
 
-    def compute(self, input):
-        return input * 2
+    def compute(self, input1, input2, operation):
+        calculator_ip.write(0x10, input1)
+        calculator_ip.write(0x18, input2)
+
+        if operation == '+':
+            return calculator_ip.read(0x20)
+        elif operation == '-':
+            return calculator_ip.read(0x30)
+        elif operation == '*':
+            return calculator_ip.read(0x40)
+        elif operation == '/':
+            return calculator_ip.read(0x50)
+        else:
+            return "Invalid operation"
+        
